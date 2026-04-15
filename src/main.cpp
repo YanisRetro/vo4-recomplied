89f524d0f70e3d993b6a5883bceb196c1f7ef002
Yanis <78453925+YanisRetro@users.noreply.github.com>
2026-04-15 07:15:43 +0100

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+
+// vo4 - ReXGlue Recompiled Project
+//
+// This file is yours to edit. 'rexglue migrate' will NOT overwrite it.
+
+#include "generated/vo4_config.h"
+#include "generated/vo4_init.h"
+
+#include "vo4_app.h"
+
+REX_DEFINE_APP(vo4, Vo4App::Create)
---
+++ src/vo4_app.h
@@ -0,0 +1,28 @@
+
+// vo4 - ReXGlue Recompiled Project
+//
+// This file is yours to edit. 'rexglue migrate' will NOT overwrite it.
+// Customize your app by overriding virtual hooks from rex::ReXApp.
+
+#pragma once
+
+#include <rex/rex_app.h>
+
+class Vo4App : public rex::ReXApp {
+ public:
+  using rex::ReXApp::ReXApp;
+
+  static std::unique_ptr<rex::ui::WindowedApp> Create(
+      rex::ui::WindowedAppContext& ctx) {
+    return std::unique_ptr<Vo4App>(new Vo4App(ctx, "vo4",
+        PPCImageConfig));
+  }
+
+  // Override virtual hooks for customization:
+  // void OnPreSetup(rex::RuntimeConfig& config) override {}
+  // void OnLoadXexImage(std::string& xex_image) override {}
+  // void OnPostSetup() override {}
+  // void OnCreateDialogs(rex::ui::ImGuiDrawer* drawer) override {}
+  // void OnShutdown() override {}
+  // void OnConfigurePaths(rex::PathConfig& paths) override {}
+};
